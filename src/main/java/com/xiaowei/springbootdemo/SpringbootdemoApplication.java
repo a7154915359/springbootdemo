f0191ded85ec5f0bd1abfb11d3c0471fe05bbba0
郭晓伟 <544399213@qq.com>
2020-03-27 11:34:43 +0800

    加盐

--- src/main/java/com/xiaowei/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/xiaowei/springbootdemo/SpringbootdemoApplication.java
@@ -1,5 +1,6 @@
 package com.xiaowei.springbootdemo;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -8,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableCaching
 @EnableScheduling
+@EnableEncryptableProperties
 public class SpringbootdemoApplication {
 
     public static void main(String[] args) {
